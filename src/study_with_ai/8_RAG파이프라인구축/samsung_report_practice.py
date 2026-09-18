b958c7a21dbc9ab84aa5f0c71ca1cc6687f5aa6b
KimByungJoo <darkping1980@gmail.com>
2026-09-18 17:50:32 +0900

    개발 중간 내역 반영

--- src/study_with_ai/8_RAG파이프라인구축/samsung_report_practice.py
+++ src/study_with_ai/8_RAG파이프라인구축/samsung_report_practice.py
@@ -9,26 +9,24 @@ from langchain_core.output_parsers import StrOutputParser           # LLM 결과
 from langchain_core.runnables import RunnablePassthrough            # 입력값을 그대로 다음 단계로 전달
 from langchain_core.prompts import ChatPromptTemplate               # Chat 모델에 전달할 프롬프트 형식을 만들기 위한 클래스 SystemMessage, HumanMessage 등을 템플릿 형태로 구성할 때 사용
 from operator import itemgetter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()                                                       # API 키 로드  
 
 model = init_chat_model("openai:gpt-5.6-luna")                      # 모델 설정
 
-debug_flag = False
-DB_PATH = "../data/practice_k_ladder_2026"                          # 임베딩 및 저장    
+debug_flag = True
+DB_PATH = "../data/practice_samsung_2026"                          # 임베딩 및 저장    
 
 SYSTME_PROMPT = """
-너는 공공 정책 안내 도우미다.
-아래 자료를 참고해서 답해라.
-참고 자료에 없으면 "자료에 없음" 이라고 말해라
-정확한 자격, 금액, 기한은 공고 확인이 필요하다고 꼭 덧붙여라
+너는 삼성전자 경영 담당자이다.
 답 끝에 참고한 페이지 번호를 [p.60] 형식으로 적어라
 """
 
 
 # pdf 파일 읽기
 def load_pdf_reader():
-    reader = PdfReader("../data/16-1_K희망사다리2026_모두의정책.pdf")
+    reader = PdfReader("../data/16-5_Samsung_Electronics_Sustainability_Report_2026_KOR.pdf")
     total_k_ladder = len(reader.pages)
     if(debug_flag):
         print("======================================================")
@@ -40,18 +38,28 @@ def load_pdf_reader():
 # pdf_docs 배열에 저장해서 리턴
 def set_pdf_docs(param_reader):
     pdf_docs = []       # pdf 리스트       
+
     # 페이지 별로 담기
     for i,  page in enumerate(param_reader.pages):
         text = page.extract_text()
         pdf_docs.append(Document(page_content=text,
                                 metadata={"source":"K희망사다리2026_모두의정책",
                                         "page": i + 1 }))
+
+
+    splitter = RecursiveCharacterTextSplitter(chunk_size=400,
+                                              chunk_overlap=80
+                                            )
+
+    pdf_chunks = splitter.split_documents(pdf_docs)
+    print("pdf_chunks :" +  len(pdf_chunks))
+
     if(debug_flag):
         print("======================================================")
-        print(pdf_docs[:10])    # 10 페이지 까지 출력        
+        print(pdf_chunks[:10])    # 10 페이지 까지 출력        
         print("======================================================")
 
-    return pdf_docs         
+    return pdf_chunks
 
 # 벡터 db 에 저장
 def insert_vector_db(param_dbpath, param_pdf_docs):
@@ -137,67 +145,70 @@ def format_docs(docs):
     return context      
 
 
-# reader = load_pdf_reader()                                          # pdf 불러오기
-# pdf_docs = set_pdf_docs(reader)                                     # pdf docs 형태의 배열에 담기 
+reader = load_pdf_reader()                                            # pdf 불러오기
+pdf_chunks = set_pdf_docs(reader)                                     # pdf docs 형태의 배열에 담기 
 # insert_vector_db(param_dbpath=DB_PATH, param_pdf_docs=pdf_docs)     # 벡터 db 에 저장
 
-load_vs = conn_vector_db(param_dbpath=DB_PATH)                        # 벡터 DB 접속  
-retreiver_mmr = get_retreiver_mmr(load_vs)                            # 검색기 가져오기  
 
 
-# rag_prompt 
-# ============================================================
-# 1. RAG에서 사용할 프롬프트 템플릿 생성
-# ============================================================
 
-rag_prompt = ChatPromptTemplate.from_messages([
-    # system: AI에게 "어떻게 답변해야 하는지" 역할/규칙을 전달  SYSTEM_PROMPT 변수에 미리 작성해 둔 내용을 사용
-    ('system', SYSTME_PROMPT),  
-    # human: 실제 사용자 질문을 만들기 위한 템플릿 {context} → 벡터 DB에서 검색한 관련 문서가 들어갈 자리 {question} → 사용자가 입력한 실제 질문이 들어갈 자리
-    ('human', '참고자료\n{context}\n\n질문\n{question}') 
-])
+# load_vs = conn_vector_db(param_dbpath=DB_PATH)                        # 벡터 DB 접속  
+# retreiver_mmr = get_retreiver_mmr(load_vs)                            # 검색기 가져오기  
 
 
-# ============================================================
-# 2. RAG Chain 구성
-# ============================================================
+# # rag_prompt 
+# # ============================================================
+# # 1. RAG에서 사용할 프롬프트 템플릿 생성
+# # ============================================================
 
-# # 질문 → 문서 검색 → 프롬프트 생성 → LLM 답변 → 문자열 반환
+# rag_prompt = ChatPromptTemplate.from_messages([
+#     # system: AI에게 "어떻게 답변해야 하는지" 역할/규칙을 전달  SYSTEM_PROMPT 변수에 미리 작성해 둔 내용을 사용
+#     ('system', SYSTME_PROMPT),  
+#     # human: 실제 사용자 질문을 만들기 위한 템플릿 {context} → 벡터 DB에서 검색한 관련 문서가 들어갈 자리 {question} → 사용자가 입력한 실제 질문이 들어갈 자리
+#     ('human', '참고자료\n{context}\n\n질문\n{question}') 
+# ])
+
+
+# # ============================================================
+# # 2. RAG Chain 구성
+# # ============================================================
+
+# # # 질문 → 문서 검색 → 프롬프트 생성 → LLM 답변 → 문자열 반환
+# # rag_chain = {
+# #     "context": (retreiver_mmr | format_docs),   # 관련 문서 검색
+# #     "question": RunnablePassthrough()            # 질문 그대로 전달
+# # } | rag_prompt | model | StrOutputParser()
+
+# # # RAG 실행
+# # result = rag_chain.invoke("청년 월세 지원 정책 찾아줘")
+
+# # 입력의 question 값을 검색에도 사용하고, 프롬프트 question에도 사용
 # rag_chain = {
-#     "context": (retreiver_mmr | format_docs),   # 관련 문서 검색
-#     "question": RunnablePassthrough()            # 질문 그대로 전달
+#     "context": itemgetter("question") | retreiver_mmr | format_docs,
+#     "question": itemgetter("question")
 # } | rag_prompt | model | StrOutputParser()
 
-# # RAG 실행
-# result = rag_chain.invoke("청년 월세 지원 정책 찾아줘")
+# # 1. 사용자로부터 직접 입력받기
+# user_input = input("2026 K희망 사다리 관련 정보를 물어봐주세요: ")
 
-# 입력의 question 값을 검색에도 사용하고, 프롬프트 question에도 사용
-rag_chain = {
-    "context": itemgetter("question") | retreiver_mmr | format_docs,
-    "question": itemgetter("question")
-} | rag_prompt | model | StrOutputParser()
-
-# 1. 사용자로부터 직접 입력받기
-user_input = input("2026 K희망 사다리 관련 정보를 물어봐주세요: ")
-
-# question이라는 이름으로 전달
-result = rag_chain.invoke({
-    "question": user_input
-})
+# # question이라는 이름으로 전달
+# result = rag_chain.invoke({
+#     "question": user_input
+# })
 
 
 
-# ============================================================
-# 4. 최종 결과 출력
-# ============================================================
+# # ============================================================
+# # 4. 최종 결과 출력
+# # ============================================================
 
-print("===============================")
-print("검색결과")
-print("===============================")
+# print("===============================")
+# print("검색결과")
+# print("===============================")
 
-# StrOutputParser()를 거쳤기 때문에
-# result는 최종적으로 문자열(str) 형태
-print(result)
+# # StrOutputParser()를 거쳤기 때문에
+# # result는 최종적으로 문자열(str) 형태
+# print(result)
 
 
 
